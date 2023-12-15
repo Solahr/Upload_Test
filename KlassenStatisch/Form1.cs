900489e733c7469d2e08d9080ddef2247d5c8ca3
Solahr <78787673+Solahr@users.noreply.github.com>
2023-12-15 22:01:11 +0100

    Upload Program

---
+++ KlassenStatisch/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KlassenStatisch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Zahl x = new Zahl(2.5);
+            Zahl p = new Zahl(-5);
+            x.MalDrei();
+            label1.Text = $"{x}\n{p}\n\n";
+
+            double y = 4.3;
+            label1.Text += $"y: {y}\n" + $"Nach Verdoppelung: {Zahl.Verdoppeln(y)}\n\n";
+
+            double r = 6.2;
+            label1.Text += $"Radius: {r}\nFläche: {r*r*Zahl.pi}";
+    }
+}
---
+++ KlassenStatisch/Zahl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlassenStatisch
+{
+    internal class Zahl
+    {
+        private double wert;
+        private readonly int nummer;
+        private static int anzahl = 0;
+        public static double pi = 3.1415926;
+
+        public Zahl(double x)
+        {
+            anzahl++;
+            nummer = anzahl;
+            wert = x;
+        }
+        public void MalDrei() => wert *= 3;
+        public static double Verdoppeln(double x) => x * 2;
+        public override string ToString() => $"Objekt Nr. {nummer}, Wert: {wert}";
+    }
+}
